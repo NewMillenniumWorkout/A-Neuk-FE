018a82843c0fc7bb9d3b7154832fedaf207cf561
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-10-14 12:35:05 +0900

    Added IconProvider.tsx

---
+++ src/utils/IconProvider.tsx
@@ -0,0 +1,17 @@
+import { ReactComponent as ChatIcon } from '../assets/icons/Chat_Circle.svg'
+import { ReactComponent as ChartIcon } from '../assets/icons/Chart_Bar_Vertical.svg'
+import { ReactComponent as LeftArrowIcon } from '../assets/icons/Chevron_Left.svg'
+import { ReactComponent as RightArrowIcon } from '../assets/icons/Chevron_Right.svg'
+import { ReactComponent as UserIcon } from '../assets/icons/User_Empty.svg'
+import { ReactComponent as CalendarIcon } from '../assets/icons/Calendar_Days.svg'
+import { ReactComponent as SettingsIcon } from '../assets/icons/Settings.svg'
+
+export const IconProvider = {
+  ChatIcon,
+  ChartIcon,
+  LeftArrowIcon,
+  RightArrowIcon,
+  UserIcon,
+  CalendarIcon,
+  SettingsIcon,
+}
