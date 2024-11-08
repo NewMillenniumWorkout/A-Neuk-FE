59ab58797fffe75255cdf1ca25940a028f599527
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-09 00:53:14 +0900

    Update tailwind.config.js : Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"black-aneuk": "#333333",
+				"white-aneuk": "#F7F4EE",
+				"text-aneuk": "#2c3e50",
+				white: "#ffffff",
+			},
+		},
+	},
+	plugins: [],
+};
