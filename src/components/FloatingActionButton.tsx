05478a343d9e97a613cac9ec02e26a562df9fa10
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-10 22:31:29 +0900

    Added FloationActionButton.tsx : Add FAB

---
+++ src/components/FloatingActionButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { IconProvider } from "../utils/IconProvider";
+
+type FloatingActionButtonProps = {};
+
+const FloatingActionButton: React.FC<FloatingActionButtonProps> = ({}) => {
+	const navigate = useNavigate();
+	return (
+		<button
+			onClick={() => {
+				navigate("/chat");
+			}}
+			className="flex justify-center items-center w-14 bg-white text-black-aneuk rounded-full shadow-lg transition duration-300 ease-in-out absolute bottom-4 right-4"
+			style={{ aspectRatio: "1 / 1" }}
+		>
+			<IconProvider.FileEditIcon className="w-[28px] h-[28px]" />
+		</button>
+	);
+};
+
+export default FloatingActionButton;
