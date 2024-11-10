63e94be7e3061a7a111bd255415759ccc1c31e17
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-10 22:32:29 +0900

    Update ChatPage.tsx, TopAppBar.tsx : ChatPage전체화면으로 변경

--- src/components/Chat/ChatPage.tsx
+++ src/components/Chat/ChatPage.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { messages, Message } from "./ChatData.ts";
 import ChatBubble from "./ChatBubble";
+import TopAppBar from "./TopAppBar.tsx";
 
 function ChatPage() {
 	return (
-		<div className="w-full p-8">
-			{messages.map((message: Message, index: number) => (
-				<ChatBubble
-					key={index}
-					content={message.content}
-					sendTime={message.send_time}
-					sender={message.chat_id}
-				/>
-			))}
+		<div className="absolute inset-0 w-full h-full bg-white">
+			<TopAppBar />
+			<div className="p-8">
+				{messages.map((message: Message, index: number) => (
+					<ChatBubble
+						key={index}
+						content={message.content}
+						sendTime={message.send_time}
+						sender={message.chat_id}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
