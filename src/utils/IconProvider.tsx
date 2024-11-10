657c603d83f679b5e9a405b35c22b08cde441c88
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-10 19:49:37 +0900

    Update IconProvider.tsx : Add FileEditIcon

--- src/utils/IconProvider.tsx
+++ src/utils/IconProvider.tsx
@@ -1,17 +1,19 @@
-import { ReactComponent as ChatIcon } from '../assets/icons/Chat_Circle.svg'
-import { ReactComponent as ChartIcon } from '../assets/icons/Chart_Bar_Vertical.svg'
-import { ReactComponent as LeftArrowIcon } from '../assets/icons/Chevron_Left.svg'
-import { ReactComponent as RightArrowIcon } from '../assets/icons/Chevron_Right.svg'
-import { ReactComponent as UserIcon } from '../assets/icons/User_Empty.svg'
-import { ReactComponent as CalendarIcon } from '../assets/icons/Calendar_Days.svg'
-import { ReactComponent as SettingsIcon } from '../assets/icons/Settings.svg'
+import { ReactComponent as ChatIcon } from "../assets/icons/Chat_Circle.svg";
+import { ReactComponent as ChartIcon } from "../assets/icons/Chart_Bar_Vertical.svg";
+import { ReactComponent as LeftArrowIcon } from "../assets/icons/Chevron_Left.svg";
+import { ReactComponent as RightArrowIcon } from "../assets/icons/Chevron_Right.svg";
+import { ReactComponent as UserIcon } from "../assets/icons/User_Empty.svg";
+import { ReactComponent as CalendarIcon } from "../assets/icons/Calendar_Days.svg";
+import { ReactComponent as SettingsIcon } from "../assets/icons/Settings.svg";
+import { ReactComponent as FileEditIcon } from "../assets/icons/File_Edit.svg";
 
 export const IconProvider = {
-  ChatIcon,
-  ChartIcon,
-  LeftArrowIcon,
-  RightArrowIcon,
-  UserIcon,
-  CalendarIcon,
-  SettingsIcon,
-}
+	ChatIcon,
+	ChartIcon,
+	LeftArrowIcon,
+	RightArrowIcon,
+	UserIcon,
+	CalendarIcon,
+	SettingsIcon,
+	FileEditIcon,
+};
