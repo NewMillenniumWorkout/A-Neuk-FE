b9b21f84d59c3bbc9ba0dc91eb30822f31558c7d
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-11 12:29:33 +0900

    Added TimeFormatter.tsx : Add time formatting

---
+++ src/utils/TimeFormatter.tsx
@@ -0,0 +1,11 @@
+export function formatTime(time: string) {
+	const date = new Date(time);
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	const period = hours < 12 ? "오전" : "오후";
+	const formattedHours = hours % 12 || 12;
+
+	return `${period} ${formattedHours.toString().padStart(2, "0")}:${minutes
+		.toString()
+		.padStart(2, "0")}`;
+}
