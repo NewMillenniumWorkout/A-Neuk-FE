11fb51573710b2e37a588469c63b07e983a8b90c
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-12 21:59:45 +0900

    Update ChatData.ts : Add long data

--- src/components/Chat/ChatData.ts
+++ src/components/Chat/ChatData.ts
@@ -32,8 +32,22 @@ export const messages: Message[] = [
 	},
 	{
 		chat_id: 123,
-		content: "코딩을 해서 재미있었구나. 어떤 코딩을 했어?",
+		content:
+			"코딩을 해서 재미있었구나. 어떤 코딩을 했어?코딩을 해서 재미있었구나. 어떤 코딩을 했어?코딩을 해서 재미있었구나. 어떤 코딩을 했어?코딩을 해서 재미있었구나. 어떤 코딩을 했어?코딩을 해서 재미있었구나. 어떤 코딩을 했어?",
 		type: "ASSISTANT",
 		send_time: "2024-11-06T12:30:00Z",
+	},
+	{
+		chat_id: 123,
+		content: "하루가 지났군",
+		type: "MEMBER",
+		send_time: "2024-11-07T12:30:00Z",
+	},
+	{
+		chat_id: 123,
+		content:
+			"동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세",
+		type: "ASSISTANT",
+		send_time: "2024-11-07T12:30:00Z",
 	},
 ];
