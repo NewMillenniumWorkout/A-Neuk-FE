e08e804e36eefcba6889d993d41db6bcef3a6d57
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-12 21:59:10 +0900

    Update TimeFormatter.tsx : Add formatDate

--- src/utils/TimeFormatter.tsx
+++ src/utils/TimeFormatter.tsx
@@ -9,3 +9,11 @@ export function formatTime(time: string) {
 		.toString()
 		.padStart(2, "0")}`;
 }
+
+export function formatDate(date: Date): string {
+	return new Intl.DateTimeFormat("ko-KR", {
+		year: "numeric",
+		month: "long",
+		day: "numeric",
+	}).format(date);
+}
