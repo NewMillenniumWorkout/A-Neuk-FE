8b5e65aecb4298d3ed410f9e19fef7aca91d4cf5
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-13 12:39:22 +0900

    Update ChatBubble.tsx : assistant bubble change

--- src/components/Chat/ChatBubble.tsx
+++ src/components/Chat/ChatBubble.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { formatTime } from "../../utils/TimeFormatter";
+import profileImg from "../../assets/images/aneuk_profile.png";
 
 type ChatBubbleProps = {
 	content: string;
@@ -15,23 +16,41 @@ const ChatBubble: React.FC<ChatBubbleProps> = ({
 	const formattedTime = formatTime(sendTime);
 	return (
 		<div
-			className={`flex ${
+			className={`flex flex-row ${
 				sender === "MEMBER" ? "justify-end" : "justify-start"
 			} items-end mb-4`}
 		>
+			{sender === "ASSISTANT" && (
+				<img
+					src={profileImg}
+					alt="Profile"
+					className="self-start w-9 h-9 mr-1"
+					style={{
+						borderRadius: "16px",
+						clipPath: "inset(0 round 40%)",
+					}}
+				></img>
+			)}
 			{sender === "MEMBER" && (
 				<div className="text-xs text-gray-aneuk pr-2">
 					{formattedTime}
 				</div>
 			)}
-			<div
-				className={`max-w-xs pt-2 pb-2.5 px-4 rounded-3xl ${
-					sender === "MEMBER"
-						? "bg-white-aneuk text-gray-800"
-						: "bg-black-aneuk text-white"
-				}`}
-			>
-				{content}
+			<div className="flex flex-col">
+				{sender === "ASSISTANT" && (
+					<div className="ml-1.5 mb-1 mt-0.5 text-black-aneuk">
+						아늑
+					</div>
+				)}
+				<div
+					className={`max-w-xs pt-2 pb-2.5 px-4 rounded-3xl ${
+						sender === "MEMBER"
+							? "bg-white-aneuk text-gray-800"
+							: "bg-black-aneuk text-white"
+					}`}
+				>
+					{content}
+				</div>
 			</div>
 			{sender === "ASSISTANT" && (
 				<div className="text-xs text-gray-aneuk pl-2">
