6578c72622c0f10a7a57321f57145aa6daff1e71
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-15 22:38:59 +0900

    Update tailwind.config.js : Add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
 				"gray-aneuk": "#cccccc",
 				white: "#ffffff",
 			},
+			keyframes: {
+				"slide-up": {
+					"0%": { opacity: "0", transform: "translateY(100%)" },
+					"100%": { opacity: "1", transform: "translateY(0)" },
+				},
+				floating: {
+					"0%, 100%": { transform: "translateY(0)" },
+					"50%": { transform: "translateY(-5px)" },
+				},
+			},
+			animation: {
+				"slide-up": "slide-up 0.5s ease-out",
+				floating: "floating 2.5s ease-in-out infinite",
+			},
 		},
 	},
 	plugins: [require("tailwind-scrollbar-hide")],
