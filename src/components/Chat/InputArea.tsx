45975bd3675d5a73cf08da2ff6908e9d99ce1260
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-15 17:16:59 +0900

    Update InputArea.tsx : Add handleKeyDown, fix IME error

--- src/components/Chat/InputArea.tsx
+++ src/components/Chat/InputArea.tsx
@@ -20,20 +20,32 @@ export function InputArea({ onSend }: { onSend: (content: string) => void }) {
 		}
 	};
 
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (e.nativeEvent.isComposing) return;
+		if (e.key === "Enter" && !e.shiftKey) {
+			e.preventDefault();
+			handleSend();
+			console.log("로그로그");
+		}
+	};
+
 	return (
-		<div className="flex flex-row h-16 min-h-16 justify-end items-end relative p-2">
+		<div className="flex flex-row justify-end items-end relative p-2 bg-white">
 			<textarea
 				ref={textareaRef}
 				placeholder="메시지를 입력하세요"
-				className="w-full max-h-40 h-11 px-4 py-2.5 mr-2 border rounded-3xl resize-none overflow-y-auto break-words focus:outline-none"
+				className="w-full max-h-32 h-11 min-h-11 px-4 py-2.5 mr-2 border rounded-3xl resize-none overflow-y-auto break-words focus:outline-none"
 				style={{
 					lineHeight: "1.5",
 					boxSizing: "border-box",
 				}}
 				rows={1}
-				onInput={autoHeight}
 				value={inputValue}
-				onChange={(e) => setInputValue(e.target.value)}
+				onChange={(e) => {
+					setInputValue(e.target.value);
+					autoHeight();
+				}}
+				onKeyDown={handleKeyDown}
 			/>
 			<SendButton onClick={handleSend} />
 		</div>
