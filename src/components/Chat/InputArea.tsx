48fddac3c4af436a2f9bc1045f815f9be30dc742
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-15 14:56:11 +0900

    Update InputArea.tsx : 자동 높이조절, 레이아웃 변경

--- src/components/Chat/InputArea.tsx
+++ src/components/Chat/InputArea.tsx
@@ -1,13 +1,28 @@
-import IconButton from "../IconButton";
 import { IconProvider } from "../../utils/IconProvider";
+import { useRef } from "react";
 
 export function InputArea() {
+	const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+	const autoHeight = () => {
+		if (textareaRef.current) {
+			textareaRef.current.style.height = "auto";
+			textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+		}
+	};
+
 	return (
-		<div className="relative p-2">
-			<input
-				type="text"
+		<div className="flex flex-row justify-end items-end relative p-2 ">
+			<textarea
+				ref={textareaRef}
 				placeholder="메시지를 입력하세요"
-				className="w-full h-10 px-4 py-2.5 border rounded-3xl"
+				className="w-full max-h-40 h-11 px-4 py-2.5 mr-2 border rounded-3xl resize-none overflow-y-auto break-words focus:outline-none"
+				style={{
+					lineHeight: "1.5",
+					boxSizing: "border-box",
+				}}
+				rows={1}
+				onInput={autoHeight}
 			/>
 			<SendButton />
 		</div>
@@ -18,7 +33,7 @@ function SendButton() {
 	return (
 		<button
 			className={
-				"flex flex-col items-center justify-center h-10 w-10 p-2 absolute top-2 right-2 bg-black-aneuk rounded-full "
+				"flex items-center justify-center h-11 w-11 min-w-11 bg-black-aneuk rounded-full "
 			}
 			onClick={() => {}}
 		>
