4cdb894b3ade92aba58a064a3b64501fbb5be8ea
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-21 01:19:28 +0900

    Update App.tsx : Add updateVh feature

--- src/App.tsx
+++ src/App.tsx
@@ -11,29 +11,42 @@ import FloatingActionButton from "./components/FloatingActionButton";
 import CalendarPage from "./components/Calendar/CalendarPage";
 import ProfilePage from "./components/Profile/ProfilePage";
 import ChartPage from "./components/Chart/ChartPage";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 	const location = useLocation();
 
 	useEffect(() => {
 		const updateVh = () => {
+			const viewportHeight =
+				window.visualViewport?.height || window.innerHeight;
 			document.documentElement.style.setProperty(
 				"--vh",
-				`${window.innerHeight * 0.01}px`
+				`${viewportHeight * 0.01}px`
 			);
 		};
 
 		updateVh();
-		window.addEventListener("resize", updateVh);
 
-		return () => window.removeEventListener("resize", updateVh);
+		if (window.visualViewport) {
+			window.visualViewport.addEventListener("resize", updateVh);
+		} else {
+			window.addEventListener("resize", updateVh);
+		}
+
+		return () => {
+			if (window.visualViewport) {
+				window.visualViewport.removeEventListener("resize", updateVh);
+			} else {
+				window.removeEventListener("resize", updateVh);
+			}
+		};
 	}, []);
 
 	return (
 		<div className="flex h-screen-dynamic w-screen justify-center items-center bg-gray-200 flex-col overflow-hidden">
-			<div className="relative flex h-full w-full sm:max-w-[440px] bg-white justify-center items-center flex-col">
-				<div className="relative w-full h-full">
+			<div className="relative flex flex-col h-screen-dynamic w-screen sm:max-w-[440px] bg-white justify-center items-center">
+				<div className="relative flex-grow w-full h-full">
 					<Routes>
 						<Route path="/calendar" element={<CalendarPage />} />
 						<Route path="/chart" element={<ChartPage />} />
