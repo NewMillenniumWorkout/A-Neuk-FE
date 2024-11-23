cf4d2d578c5e117a5c56685209378a6afc4417a9
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-11-24 01:29:49 +0900

    Added EmotionSelectPage.tsx

---
+++ src/components/EmotionSelect/EmotionSelectPage.tsx
@@ -0,0 +1,9 @@
+const EmotionSelectPage = () => {
+	return (
+		<div className="absolute inset-0 bg-white flex flex-col overflow-hidden">
+			감정 선택 페이지
+		</div>
+	);
+};
+
+export default EmotionSelectPage;
