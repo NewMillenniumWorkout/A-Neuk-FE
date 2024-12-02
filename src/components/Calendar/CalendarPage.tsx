9c93a0d3bc715dacbe343efac6a62c7164dace66
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-02 22:18:22 +0900

    Update CalendarPage.tsx : Add shadow

--- src/components/Calendar/CalendarPage.tsx
+++ src/components/Calendar/CalendarPage.tsx
@@ -6,7 +6,7 @@ function CalendarPage() {
 
 	return (
 		<div className="flex flex-col w-full h-full">
-			<div className="flex-shrink-0 w-full px-8 py-4">
+			<div className="flex-shrink-0 w-full px-8 py-4 z-20 shadow-md">
 				<WeeklyCalendar
 					mode="single"
 					selected={date}
@@ -14,7 +14,7 @@ function CalendarPage() {
 					className="w-full"
 				/>
 			</div>
-			<div className="flex flex-col flex-grow justify-start items-center bg-white-aneuk overflow-y-auto">
+			<div className="flex flex-col flex-grow justify-start items-center bg-white-aneuk overflow-y-auto pb-24">
 				<div className="flex flex-col w-[90%] aspect-[2/2.8] p-5 mt-[5%] bg-white rounded-[32px] shadow-custom-strong"></div>
 				<div className="flex flex-col w-[90%] aspect-[2/2.8] p-5 mt-[5%] bg-white rounded-[32px] shadow-custom-strong"></div>
 			</div>
