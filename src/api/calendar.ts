199007b191da060128654917d76b1af03a06b04f
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-04 04:29:41 +0900

    Update calendar.ts : Add api

--- src/api/calendar.ts
+++ src/api/calendar.ts
@@ -1,4 +1,5 @@
 import apiClient from "./index";
+import { FinalDiary } from "./diary";
 
 export const API_CALENDAR = {
 	getMonthDiary: async (month: string) => {
@@ -6,10 +7,35 @@ export const API_CALENDAR = {
 			const response = await apiClient.get(
 				`/get-diary/month?month=${month}`
 			);
-			console.log(response.data);
 			return response.data;
 		} catch (error: any) {
 			console.error("Error get monthly diaries:", error.message);
+			throw error;
+		}
+	},
+	getDateDiary: async (date: string): Promise<FinalDiary> => {
+		try {
+			console.log(date);
+			const response = await apiClient.get<FinalDiary>(
+				`/get-diary/day?date=${date}`
+			);
+			console.log(response.data);
+			return response.data;
+		} catch (error: any) {
+			console.error("Error get date diary:", error.message);
+			throw error;
+		}
+	},
+	getDiaryByDiaryId: async (diaryId: number): Promise<FinalDiary> => {
+		try {
+			console.log(diaryId);
+			const response = await apiClient.get<FinalDiary>(
+				`/get-diary/diary-id?diary-id=${diaryId}`
+			);
+			console.log(response.data);
+			return response.data;
+		} catch (error: any) {
+			console.error("Error get diary by diary id:", error.message);
 			throw error;
 		}
 	},
