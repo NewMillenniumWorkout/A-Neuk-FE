fc8f208650ecc92c8fa3db2dced4c650d4943783
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-03 17:02:58 +0900

    Update diary.ts : Add genNewContent

--- src/api/diary.ts
+++ src/api/diary.ts
@@ -1,6 +1,14 @@
 import { EmotionSelectData } from "../components/EmotionSelect/EmotionSelectData";
 import apiClient from "./index";
 
+interface NewContent {
+	data: {
+		paragraph_id: number;
+		order_index: number;
+		final_content: string;
+	};
+}
+
 export const API_DIARY = {
 	getEmotions: async (chatId: number): Promise<EmotionSelectData> => {
 		try {
@@ -10,6 +18,30 @@ export const API_DIARY = {
 			return response.data;
 		} catch (error: any) {
 			console.error("Error generating first diary:", error.message);
+			throw error;
+		}
+	},
+	genNewContent: async (
+		diary_id: number,
+		order_index: number,
+		original_content: string,
+		emotions: string[]
+	): Promise<NewContent> => {
+		try {
+			const selectData = {
+				diary_id: diary_id,
+				order_index: order_index,
+				original_content: original_content,
+				emotions: emotions,
+			};
+			const response = await apiClient.post<NewContent>(
+				`/diary/emotion/select`,
+				selectData
+			);
+			console.log(response);
+			return response.data;
+		} catch (error: any) {
+			console.error("Error generating new content:", error.message);
 			throw error;
 		}
 	},
