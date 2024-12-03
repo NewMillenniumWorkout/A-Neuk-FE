de9ea0804e5aa8e282f9e9bc35148160da2b8dc0
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-04 00:43:05 +0900

    Update TimeFormatter.tsx : Add formatToYYYYMM

--- src/utils/TimeFormatter.tsx
+++ src/utils/TimeFormatter.tsx
@@ -19,3 +19,9 @@ export function formatDate(serverDate: Date): string {
 		day: "numeric",
 	}).format(koreanDate);
 }
+
+export const formatToYYYYMM = (date: Date): string => {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	return `${year}-${month}`;
+};
