31b0b679f615dbdd25de757f9386f3fa4ba340aa
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-04 04:30:58 +0900

    Update CalendarPage.tsx : Get diary success

--- src/components/Calendar/CalendarPage.tsx
+++ src/components/Calendar/CalendarPage.tsx
@@ -3,28 +3,45 @@ import { WeeklyCalendar } from "./WeeklyCalendar";
 import Card from "./Card";
 import { API_CALENDAR } from "../../api/calendar";
 import { formatToYYYYMM, formatToYYYYMMDD } from "../../utils/TimeFormatter";
+import { FinalDiary } from "../../api/diary";
 
-function CalendarPage() {
+const CalendarPage: React.FC = () => {
 	const [date, setDate] = useState<Date | undefined>(new Date());
 	const [diaryDates, setDiaryDates] = useState<string[]>([]);
+	const [curDiaryId, setCurDiaryId] = useState<number | null>(null);
+	const [curDiary, setCurDiary] = useState<FinalDiary | null>(null);
 
 	useEffect(() => {
 		const loadMonthlyDiaries = async () => {
 			if (!date) return;
-			const month = formatToYYYYMM(date);
+			const formattedMonth = formatToYYYYMM(date);
 			try {
-				const response = await API_CALENDAR.getMonthDiary(month);
-				console.log("API Response:", response);
+				const response = await API_CALENDAR.getMonthDiary(
+					formattedMonth
+				);
 				const diaries = response.data.diaries_with_diary || [];
 				const diaryDates = diaries.map((diary: any) =>
 					formatToYYYYMMDD(new Date(diary.month))
 				);
+				setCurDiaryId(diaries[diaries.length - 1].diary_id);
 				setDiaryDates(diaryDates);
 			} catch (error: any) {
 				console.error("Error loading monthly diaries: ", error);
 			}
 		};
+		const loadDateDiary = async () => {
+			if (!date) return;
+			const formattedDate = formatToYYYYMMDD(date);
+			try {
+				const response = await API_CALENDAR.getDateDiary(formattedDate);
+				console.log("API Response:", response);
+				setCurDiary(response);
+			} catch (error: any) {
+				console.error("Error loading date diary: ", error);
+			}
+		};
 		loadMonthlyDiaries();
+		loadDateDiary();
 	}, [date]);
 
 	return (
@@ -32,8 +49,8 @@ function CalendarPage() {
 			<div className="flex-shrink-0 w-full px-4 pt-4 pb-6 z-20 rounded-b-xl bg-white shadow-md">
 				<WeeklyCalendar
 					mode="single"
-					selected={date}
-					onSelect={setDate}
+					selectedDate={date}
+					onDateChange={setDate}
 					className="w-full"
 					diaryDates={diaryDates}
 				/>
@@ -43,6 +60,6 @@ function CalendarPage() {
 			</div>
 		</div>
 	);
-}
+};
 
 export default CalendarPage;
