36e05faf9c5c4deb85f00853a0497b78493a7893
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-04 02:35:28 +0900

    Update CalendarPage.tsx : 일기 있는 날짜 정보 포맷팅

--- src/components/Calendar/CalendarPage.tsx
+++ src/components/Calendar/CalendarPage.tsx
@@ -1,11 +1,12 @@
+import React, { useEffect, useState } from "react";
 import { WeeklyCalendar } from "./WeeklyCalendar";
 import Card from "./Card";
-import React, { useEffect, useState } from "react";
 import { API_CALENDAR } from "../../api/calendar";
-import { formatToYYYYMM } from "../../utils/TimeFormatter";
+import { formatToYYYYMM, formatToYYYYMMDD } from "../../utils/TimeFormatter";
 
 function CalendarPage() {
-	const [date, setDate] = React.useState<Date | undefined>(new Date());
+	const [date, setDate] = useState<Date | undefined>(new Date());
+	const [diaryDates, setDiaryDates] = useState<string[]>([]); // 일기 있는 날짜 저장
 
 	useEffect(() => {
 		const loadMonthlyDiaries = async () => {
@@ -13,9 +14,16 @@ function CalendarPage() {
 			const month = formatToYYYYMM(date);
 			try {
 				const response = await API_CALENDAR.getMonthDiary(month);
-				console.log(response);
+				console.log("API Response:", response); // 응답 데이터 확인
+
+				// 일기가 있는 날짜 추출
+				const diaries = response.data.diaries_with_diary || [];
+				const diaryDates = diaries.map((diary: any) =>
+					formatToYYYYMMDD(new Date(diary.month))
+				);
+				setDiaryDates(diaryDates);
 			} catch (error: any) {
-				console.error("Error load monthly diaries: ", error);
+				console.error("Error loading monthly diaries: ", error);
 			}
 		};
 		loadMonthlyDiaries();
@@ -29,6 +37,7 @@ function CalendarPage() {
 					selected={date}
 					onSelect={setDate}
 					className="w-full"
+					diaryDates={diaryDates} // 일기 날짜 전달
 				/>
 			</div>
 			<div className="flex flex-col flex-grow justify-start items-center bg-white-aneuk overflow-y-auto pb-24">
