a85a522c016a163b0aeadb1c70baed5a62703aaf
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-04 23:51:44 +0900

    Added chart.ts : Chart API test complete

--- src/components/Chart/ChartPage.tsx
+++ src/components/Chart/ChartPage.tsx
@@ -1,7 +1,29 @@
 import React, { useState } from "react";
+import { API_CHART } from "../../api/chart";
 
-function ChartPage() {
-	return <div className="w-full h-full p-8 bg-cyan-100"></div>;
-}
+const ChartPage: React.FC = () => {
+	const handleCollectionButton = async () => {
+		await API_CHART.getCollection();
+	};
+	const handleMonthButton = async () => {
+		await API_CHART.getLastMonth();
+	};
+	return (
+		<div className="flex flex-col space-y-8 justify-center items-center w-full h-full bg-white-aneuk p-8 bg-blue-700-100">
+			<button
+				className="bg-black-aneuk text-white font-pretendard-bold p-5 rounded-2xl shadow-xl"
+				onClick={handleCollectionButton}
+			>
+				도감
+			</button>
+			<button
+				className="bg-black-aneuk text-white font-pretendard-bold p-5 rounded-2xl shadow-xl"
+				onClick={handleMonthButton}
+			>
+				최근 30일
+			</button>
+		</div>
+	);
+};
 
 export default ChartPage;
