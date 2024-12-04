caaa2e19e5cbf414cc1f4373533d383194625de3
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-04 23:42:38 +0900

    Update HomePage.tsx : Api test complete

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
+import apiClient from "../../api";
+import { FinalDiary } from "../../api/diary";
+import Card from "../Calendar/Card";
 
-function HomePage() {
-	return <div className="w-full h-full p-8 bg-blue-700-100"></div>;
-}
+const HomePage: React.FC = () => {
+	const [randomDiary, setRandomDiary] = useState<FinalDiary | null>(null);
+	const handleRandomButton = () => {
+		const getRandomDiary = async () => {
+			try {
+				const response = await apiClient.get("/home/random");
+				console.log(response);
+				setRandomDiary(response.data);
+			} catch (error: any) {
+				console.error("Error getting random diary:", error.message);
+				throw error;
+			}
+		};
+		getRandomDiary();
+	};
+	return (
+		<div className="flex flex-col space-y-8 justify-center items-center w-full h-full bg-white-aneuk p-8 bg-blue-700-100">
+			<Card curDiary={randomDiary} />
+			<button
+				className="bg-black-aneuk text-white font-pretendard-bold p-5 rounded-2xl shadow-xl"
+				onClick={handleRandomButton}
+			>
+				행복버튼
+			</button>
+		</div>
+	);
+};
 
 export default HomePage;
