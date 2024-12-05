a39f8077613bda421ed445402e801d408a908ee4
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-06 03:39:09 +0900

    Update App.tsx : Change FAB

--- src/App.tsx
+++ src/App.tsx
@@ -62,10 +62,9 @@ function App() {
 						<Route path="/chart" element={<ChartPage />} />
 						<Route path="/profile" element={<ProfilePage />} />
 					</Routes>
-					{location.pathname !== "/home" &&
-						location.pathname !== "/profile" && (
-							<FloatingActionButton />
-						)}
+					{location.pathname === "/calendar" && (
+						<FloatingActionButton />
+					)}
 				</div>
 
 				{location.pathname === "/login" && <LoginPage />}
