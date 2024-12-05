5187433347267e0592b61496a33db8550d68ef6c
김민식 <minsik@gimminsig-ui-MacBookPro.local>
2024-12-06 03:24:13 +0900

    Update HomePage.tsx : 행복버튼 is gone

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -4,13 +4,15 @@ import { FinalDiary } from "../../api/diary";
 import Card from "../Calendar/Card";
 import HomeCard from "./HomeCard";
 import { EmotionLabels } from "../Calendar/EmotionLabels";
+import logoImg from "../../assets/images/logo.png";
+import logoClickedImg from "../../assets/images/logo_close.png";
 
 const HomePage: React.FC = () => {
 	const [randomDiary, setRandomDiary] = useState<FinalDiary | null>(null);
 	const [isLoading, setIsLoading] = useState(true);
 	const [isFlipped, setIsFlipped] = useState(false);
 	const [isFlipping, setIsFlipping] = useState(false);
-
+	const [isImageClicked, setIsImageClicked] = useState(false);
 	const [selectedEmotionId, setSelectedEmotionId] = useState<number | null>(
 		null
 	);
@@ -64,6 +66,15 @@ const HomePage: React.FC = () => {
 		}
 	}, [selectedEmotionId]);
 
+	const handleMouseDown = () => {
+		setIsImageClicked(true);
+	};
+
+	const handleMouseUp = () => {
+		setIsImageClicked(false);
+		handleRandomButton();
+	};
+
 	return (
 		<div
 			ref={containerRef}
@@ -97,12 +108,14 @@ const HomePage: React.FC = () => {
 					/>
 				)}
 				{!isFlipped && (
-					<button
-						className="flex bg-black-aneuk text-white font-pretendard-bold p-5 mt-12 rounded-2xl shadow-xl"
-						onClick={handleRandomButton}
-					>
-						행복버튼
-					</button>
+					<img
+						src={isImageClicked ? logoClickedImg : logoImg}
+						onMouseDown={handleMouseDown}
+						onMouseUp={handleMouseUp}
+						className={`p-5 mt-12 object-contain cursor-pointer ${
+							isImageClicked ? "w-[28%]" : "w-[30%]"
+						}`}
+					></img>
 				)}
 			</div>
 		</div>
